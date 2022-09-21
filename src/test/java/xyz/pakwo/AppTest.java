e43972970bfc38e5ace8b820e029410996f1eb66
Sarwo Wibowo <sarwo04@gmail.com>
2022-09-21 14:57:07 +0800

    initial commit

---
+++ src/test/java/xyz/pakwo/AppTest.java
@@ -0,0 +1,10 @@
+package xyz.pakwo;
+
+import org.junit.runner.RunWith;
+
+@RunWith(io.cucumber.junit.Cucumber.class)
+@io.cucumber.junit.CucumberOptions(features = "src/test/resources")
+public class AppTest
+{
+
+}
